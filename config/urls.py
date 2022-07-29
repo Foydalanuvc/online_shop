aa900111757d23d80317b2dadec5412ad25f5f62
dovudbek123 <dovudbek20071404@gmail.com>
2022-07-29 11:27:33 +0500

    color input added

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('blog/', include('blogs.urls')),
--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import ProductTegModel, ProductModel, CategoryModel, SizeModel, ColorModel, BrandModel
+from .forms import ColorModelAdminForms
 
 
 @admin.register(ProductTegModel)
@@ -25,9 +28,14 @@ class BrandModelAdmin(admin.ModelAdmin):
 
 @admin.register(ColorModel)
 class ColorModelAdmin(admin.ModelAdmin):
-    list_display = ['code']
+    list_display = ['code', 'color']
     list_display_links = ['code']
     search_fields = ['code']
+    form = ColorModelAdminForms
+
+    def color(self, obj):
+        free_space = '&nbsp;' * 5
+        return mark_safe(f"<div style='background-color:{obj.code}; width:200px'>{free_space}</div>")
 
 
 @admin.register(CategoryModel)
@@ -35,6 +43,7 @@ class CategoryModelAdmin(admin.ModelAdmin):
     list_display = ['name']
     list_display_links = ['name']
     search_fields = ['name']
+    form = ColorModelAdminForms
 
 
 @admin.register(ProductModel)
